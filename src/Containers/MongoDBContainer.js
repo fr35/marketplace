7221b38c90334e1758deab4c218647ccaa1debd9
Facundo Rodriguez <facundorodriguezgranieri@gmail.com>
2022-11-19 00:41:44 -0300

    first

---
+++ src/Containers/MongoDBContainer.js
@@ -0,0 +1,31 @@
+class MongoDBContainer {
+    constructor() {
+    
+    }
+
+    async getAll() {
+    
+    }
+
+    async save(element) {
+    
+    }
+
+    async getById(id) {
+    
+    }
+
+    async deleteById(id) {
+    
+    }
+
+    async deleteAll() {
+    
+    }
+
+    async updateById(id, newData) {
+    
+    }
+}
+
+export {MongoDBContainer}
